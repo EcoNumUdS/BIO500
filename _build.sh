1e9d62be82e4e882e5c58b4da163258bf2b551e2
Steve Vissault <s.vissault@yahoo.fr>
2018-02-15 14:43:41 -0500

    Change build script

--- _build.sh
+++ _build.sh
@@ -4,6 +4,6 @@ for dir in $(ls -d cours*); do
     cd $dir
     rm -rf ./pres/.cache
     cat *.Rmd > ./pres/index.Rmd
-    Rscript --no-site-file --no-init-file ./scripts/get_slidify.R
+    Rscript -e "slidify::slidify('./pres/index.Rmd')"
     cd ..
 done
