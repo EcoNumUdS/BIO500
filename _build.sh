9f2563eecae2d043eba382bfe48af2d95c99fe29
Willian <willian.vieira@usherbrooke.ca>
2020-02-10 19:10:07 -0500

    add bloc1

--- _build.sh
+++ _build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-for dir in $(ls -d cours*); do
+for dir in $(ls -d bloc*); do
     cd $dir
     cat *.Rmd | grep -v index.Rmd > index.Rmd
     Rscript -e "rmarkdown::render('index.Rmd')"
--- cours2/scripts/get_slidify.R
+++
@@ -1,9 +0,0 @@
-if(Sys.getenv('TRAVIS') != TRUE){
-  if (!require(devtools)) install.packages("devtools")
-  if (!require(slidify)) devtools::install_github("ramnathv/slidify"); devtools::install_github("ramnathv/slidifyLibraries")
-
-  ## Load package slidify & DiagrammeR
-  library(slidify)
-}
-
-slidify::slidify('./pres/index.Rmd')
