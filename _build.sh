67f77110fc676ed381513c1062a20dba3ae3f4b6
Willian <willian.vieira@usherbrooke.ca>
2020-02-11 10:13:03 -0500

    💚forgot assets

--- _build.sh
+++ _build.sh
@@ -10,8 +10,13 @@ for dir in $(ls -d bloc*); do
 done
 
 # Prepare files to deployment
+# Send all assets and html files to the slides folder
+# Travis will send the slides folder to the gh-pages branch
 for dir in $(ls -d bloc*); do
     mkdir -p ./slides/$dir
     mv ./$dir/index.html ./slides/$dir
     mv ./$dir/assets ./slides/$dir
 done
+
+# Send layout assets to slides folder
+mv ./assets ./slides
