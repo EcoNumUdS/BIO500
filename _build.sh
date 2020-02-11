0d5f66ea5f8b1f6ec9680ffd85ee3909aada7ed4
Willian <willian.vieira@usherbrooke.ca>
2020-02-11 09:18:34 -0500

    💚 travis

--- _build.sh
+++ _build.sh
@@ -1,9 +1,17 @@
 #!/bin/sh
 
+# Compile Rmd in html pages
 for dir in $(ls -d bloc*); do
     cd $dir
     cat *.Rmd | grep -v index.Rmd > index.Rmd
     Rscript -e "rmarkdown::render('index.Rmd')"
     rm index.Rmd
     cd ..
+done
+
+# Prepare files to deployment
+for dir in $(ls -d bloc*); do
+    mkdir -p ./slides/$dir
+    mv ./$dir/index.html ./slides/$dir
+    mv ./$dir/assets ./slides/$dir
 done
