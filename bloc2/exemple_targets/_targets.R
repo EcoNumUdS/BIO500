2efdae456fd2a1976a4761053c6aa91d0befab95
DominiqueGravel <dominique.gravel@usherbrooke.ca>
2022-04-19 11:58:37 -0400

    modif sur exemple targets

--- bloc2/exemple_targets/_targets.R
+++ bloc2/exemple_targets/_targets.R
@@ -5,15 +5,20 @@ source("R/figure.R")
 tar_option_set(packages = c("MASS", "igraph"))
 list(
   tar_target(
-    data, # Le nom de l'objet
-    read.table("data/data.txt", header = T) # Lecture du fichier
+    name = path, # Cible
+    command = "data/data.txt", # Emplacement du fichier
+    format = "file"
   ), 
+  tar_target(
+    name = data, # Cible pour l'objet de données
+    command = read.table(path) # Lecture des données
+  ),   
   tar_target(
     resultat_modele, # Cible pour le modèle 
     mon_modele(data) # Exécution de l'analyse
-#  ),
-#  tar_target(
-#    figure, # Cible pour l'exécution de la figure
-#    ma_figure(data, resultat_modele) # Réalisation de la figure
+  ),
+  tar_target(
+    figure, # Cible pour l'exécution de la figure
+    ma_figure(data, resultat_modele) # Réalisation de la figure
   )
 )
